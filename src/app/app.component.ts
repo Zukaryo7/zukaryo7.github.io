f808db42b076637879baccbc5755eca7e78ffa8f
zukaryo <zukaryo7@gmail.com>
2021-06-07 15:11:40 +0200

    First

---
+++ src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'kanas';
+  kana = "";
+  kanaType = "";
+  hiragana = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to",
+              "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri",
+              "ru", "re", "ro", "wa", "wo", "n"];
+  katakana = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to",
+              "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri",
+              "ru", "re", "ro", "wa", "wo", "n"];
+  reinitializationMessage = "";
+  counter = 0;
+
+  constructor() {
+    this.generateKana();
+  }
+
+  generateKana() {
+    let isHiragana = this.selectKanaType();
+    if(this.kana !== "Terminé !") {
+      this.getKana(isHiragana);
+    }
+  }
+
+  selectKanaType() {
+    if(this.katakana.length == 0 && this.hiragana.length == 0) {
+      this.kana = "Terminé !";
+      this.kanaType = "";
+      return false;
+    } else if(this.hiragana.length == 0) {
+      return false;
+    } else if(this.katakana.length == 0) {
+      return true;
+    } else {
+      return Math.random() < 0.5;
+    }
+  }
+
+  getKana(isHiragana : boolean) {
+    if(isHiragana) {
+      let indice = Math.floor(Math.random() * this.hiragana.length);
+      this.kana = this.hiragana[indice];
+      this.hiragana.splice(indice, 1);
+      this.kanaType = "Hiragana";
+    } else {
+      let indice = Math.floor(Math.random() * this.katakana.length);
+      this.kana = this.katakana[indice];
+      this.katakana.splice(indice, 1);
+      this.kanaType = "Katakana";
+    }
+  }
+
+
+  reinitialize() {
+    this.counter++;
+    this.hiragana = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to",
+                     "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri",
+                     "ru", "re", "ro", "wa", "wo", "n"];
+    this.katakana = ["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta", "chi", "tsu", "te", "to",
+                     "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho", "ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri",
+                     "ru", "re", "ro", "wa", "wo", "n"];
+    this.reinitializationMessage = "Liste réinitialisée ! (" + this.counter + ")";
+    this.kana = "";
+    this.generateKana();
+  }
+
+}
