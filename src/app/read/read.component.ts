dfae0466ad130fdaa75dcde8e171adb2b8ecf084
zukaryo <zukaryo7@gmail.com>
2021-07-03 18:05:51 +0200

    finishing reading module

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { toRomaji } from 'wanakana';
+import { toRomaji, toKatakana, toHiragana } from 'wanakana';
 import { Chronometer } from 'ngx-chronometer';
 import { addClass, replaceClass, removeClass } from 'src/environments/dom';
+import { KANAS } from 'src/environments/kanaData';
 
 @Component({
   selector: 'app-read',
@@ -14,9 +15,19 @@ export class ReadComponent {
   wordFoundIndex = 0;
   totalWordsFound = 0;
   totalWordsFail = 0;
+  lastFailed = "";
   chronometer: Chronometer = new Chronometer();
-  chronoStarted = false;
   chronoPaused = true;
+  isHiraganaChecked = true;
+  isKatakanaChecked = true;
+  hiraganaDisabled = false;
+  katakanaDisabled = false;
+  wpm = 0;
+  wpmInterval = setInterval(() => {
+    if(this.chronometer !== undefined && this.chronometer.second > 0) {
+      this.wpm = (this.totalWordsFound / this.chronometer.second) * 60;
+    }
+  }, 1000);
 
   constructor() {
     this.generateNewTab();
@@ -26,28 +37,32 @@ export class ReadComponent {
     this.prepareNextWord();
   }
 
+  ngOnDestroy() {
+    clearInterval(this.wpmInterval);
+  }
+
   onKeyUp(changement: any) {
+    this.startChrono();
     let inputStr = changement.target.value.trim().toLocaleLowerCase();
     let toFound = toRomaji(this.selectedWords[this.wordFoundIndex]);
     if(inputStr === "" || inputStr.length > toFound.length)
       this.resetInput(changement);
     else if(inputStr.length === toFound.length) {
-        inputStr === toFound ? this.updateWordStatus("word-found") : this.updateWordStatus("word-fail");
+        if(inputStr !== toFound) this.lastFailed = `(${toFound})`; 
+        inputStr === toFound ? this.updateWord("word-found") : this.updateWord("word-fail");
         this.resetInput(changement);
         this.wordFoundIndex++;
         if(this.wordFoundIndex === this.selectedWords.length) {
-          this.resetCss();
-          this.wordFoundIndex = 0;
-          this.generateNewTab();
+          this.resetTab();
         }
         this.prepareNextWord();
     }
   }
 
-  updateWordStatus(status : string) {
+  updateWord(status : string) {
+    removeClass("word-"+ (this.wordFoundIndex + 1), "word-current");
     addClass(status, "word-" + (this.wordFoundIndex + 1));
     status === "word-found" ? this.totalWordsFound++ : this.totalWordsFail++;
-    removeClass("word-"+ (this.wordFoundIndex + 1), "word-current");
   }
 
   prepareNextWord() {
@@ -58,21 +73,46 @@ export class ReadComponent {
     inputValue.target.value = "";
   }
 
-
-
-
-  
-  // Vider puis remplir le tableau selectedWords avec des mots randoms du fichier "../../assets/res/jap.txt"
-  // Les mots ne doivent pas contenir de kanji (remplacés par des hiragana ou katakana correspondants ou furigana)
-  // Soit changer le txt et remplacer tous les kanjis soit utiliser un truc et le faire ici à la volée.
-  // La méthode est appelée automatiquement au chargement de la page ou lorsque le tableau est complété par l'utilisateur
-  generateNewTab() {
-    this.selectedWords = ["という", "という", "という", "という", "という", "という", "という"];
+  updateHiragana() {
+    this.isHiraganaChecked = !this.isHiraganaChecked
+    if(this.katakanaDisabled) this.katakanaDisabled = false; else this.katakanaDisabled = true;
+    this.resetTab();
   }
 
+  updateKatakana() {
+    this.isKatakanaChecked = !this.isKatakanaChecked
+    if(this.hiraganaDisabled) this.hiraganaDisabled = false; else this.hiraganaDisabled = true;
+    this.resetTab();
+  }
 
+  generateNewTab() {
+    this.selectedWords = [];
+    for(let i = 0; i < 20; i++) {
+      let word = "";
+      let syllableNbr = Math.floor(Math.random() * 3) + 2;
+      for(let j = 0; j < syllableNbr; j++) {
+        let kanaIndex = Math.floor(Math.random() * 46);
+        word += KANAS.baseKanas[kanaIndex];
+      }
+      word = this.convertWordToKana(word);
+      this.selectedWords.push(word);
+    }
+  }
 
+  convertWordToKana(word : string) {
+    if(this.isHiraganaChecked && this.isKatakanaChecked)
+      return Math.random() < 0.5 ? toHiragana(word) : toKatakana(word);
+    else if(this.isHiraganaChecked)
+      return toHiragana(word);
+    else 
+      return toKatakana(word);
+  }
 
+  resetTab() {
+    this.resetCss();
+    this.wordFoundIndex = 0;
+    this.generateNewTab();
+  }
 
   resetCss() {
     for(let i = 1; i <= this.wordFoundIndex; i++) {
@@ -82,9 +122,8 @@ export class ReadComponent {
   }
 
   startChrono() {
-    if(!this.chronoStarted) {
+    if(this.chronoPaused) {
       this.chronometer.start();
-      this.chronoStarted = true;
       this.chronoPaused = false;
       replaceClass("pause", "fa-play", "fa-pause");
     }
